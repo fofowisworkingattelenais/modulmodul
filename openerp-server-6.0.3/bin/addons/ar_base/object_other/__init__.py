a96a67c15f84324e5d7a88f24074d32531787119
Faris Bassam <fofowx@gmail.com>
2014-05-12 14:22:15 +0700

    ar_base

---
+++ openerp-server-6.0.3/bin/addons/ar_base/object_other/__init__.py
@@ -0,0 +1,37 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution	
+#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>). All Rights Reserved
+#    $Id$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+import res_company
+import ir_sequence
+import res_currency
+import res_country
+import res_country_state
+import res_bank
+import res_partner
+#import res_partner_function
+import res_partner_category
+import res_partner_title
+import res_partner_address
+import res_partner_bank
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
